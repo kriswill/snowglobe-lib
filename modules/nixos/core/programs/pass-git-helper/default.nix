bcb5196560d9f41f0bad20e857774273a7a2f7f0
EarthGman <EarthGman@protonmail.com>
2025-11-18 16:05:01 -0600

    add pass git helper to programs

---
+++ modules/nixos/core/programs/pass-git-helper/default.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  program-name = "pass-git-helper";
+  cfg = config.programs.${program-name};
+in
+{
+  options.programs.${program-name} = lib.mkProgramOption {
+    description = "git credential integration with pass";
+    programName = program-name;
+    packageName = program-name;
+    inherit pkgs;
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [
+      cfg.package
+    ];
+  };
+}
