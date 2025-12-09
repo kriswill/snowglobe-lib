681d44d95f16479ea64d22523d515009295650e1
EarthGman <EarthGman@protonmail.com>
2025-12-08 18:44:18 -0600

    update flake

--- packages/default.nix
+++ packages/default.nix
@@ -4,9 +4,6 @@ let
 in
 {
   nixos-grub = callPackage ./grub-themes/nixos.nix { };
-  userchrome-toggle-extended = callPackage ./firefox-extensions/uct-extended.nix { };
-  betterfox = callPackage ./firefox-themes/betterfox.nix { };
-  shyfox = callPackage ./firefox-themes/shyfox.nix { };
   omori-font = callPackage ./fonts/omori-font.nix { inherit inputs; };
   "8-bit-operator-font" = callPackage ./fonts/8-bit-operator-jve.nix { inherit inputs; };
   mov-cli-youtube = callPackage ./mov-cli-plugins/youtube.nix { };
