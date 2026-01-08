54ee75b0e6c6fe86ece54c3fabcb821983de9c8e
EarthGman <EarthGman@protonmail.com>
2026-01-07 21:24:43 -0600

    improvement to kanata jp106

--- nixosModules/mixins/kanata/jp106/default.nix
+++ nixosModules/mixins/kanata/jp106/default.nix
@@ -8,15 +8,25 @@ let
   cfg = config.gman.kanata.jp106;
 in
 {
-  options.gman.kanata.jp106.enable =
-    lib.mkEnableOption "gman's personal keymap for a jp106 keyboard layout";
+  options.gman.kanata.jp106 = {
+    enable = lib.mkEnableOption "gman's personal keymap for a jp106 keyboard layout";
+    config.devices = lib.mkOption {
+      description = ''
+        paths to keyboard devices which will be affected by this kanata configuration.
+      '';
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
+  };
+
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.kanata ];
+    environment.systemPackages = [ config.services.kanata.package ];
     services.kanata = {
       enable = true;
       keyboards = {
-        default = {
-          extraDefCfg = "process-unmapped-keys yes";
+        gman-jp106 = {
+          devices = cfg.config.devices;
+          extraDefConfig = "process-unmapped-keys yes";
           config = builtins.readFile ./config.kbd;
         };
       };
