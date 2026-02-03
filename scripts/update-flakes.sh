33b1738cbe5bbb2b1272e2dae08095a6ea8911a0
EarthGman <EarthGman@protonmail.com>
2026-02-03 08:32:00 -0600

    update flake

--- scripts/update-flakes.sh
+++ scripts/update-flakes.sh
@@ -2,11 +2,12 @@
 
 # update flakes of various projects to ensure that all inputs are in sync with this repository.
 PROJECTS_DIR=~/src/git
-PROJECT_DIRS=
-"$PROJECTS_DIR/earthgman.dev/earthgman/nix-modules"
-"$PROJECTS_DIR/earthgman.dev/earthgman/nixos-hosts"
-"$PROJECTS_DIR/earthgman.dev/earthgman/dotfiles"
+PROJECT_DIRS="$PROJECTS_DIR/earthgman.dev/earthgman/nix-modules
+$PROJECTS_DIR/earthgman.dev/earthgman/nixos-hosts
+$PROJECTS_DIR/earthgman.dev/earthgman/dotfiles"
 
 for project in $PROJECT_DIRS; do
-	nix flake update "$project"
+	cd "$project" || exit 1
+	pwd
+	nix flake update
 done
