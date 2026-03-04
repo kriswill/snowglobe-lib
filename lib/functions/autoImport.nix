52391400588795479c9e582d036405cb05c6605f
EarthGman <EarthGman@protonmail.com>
2026-03-04 14:44:06 -0600

    prepare for v0.9 merge

--- lib/functions/autoImport.nix
+++
@@ -1,7 +0,0 @@
-{ lib, ... }:
-dir:
-let
-  fileNames = builtins.attrNames (builtins.readDir dir);
-  strippedFileNames = lib.filter (name: name != "default.nix") fileNames;
-in
-lib.forEach (strippedFileNames) (fileName: dir + /${fileName})
