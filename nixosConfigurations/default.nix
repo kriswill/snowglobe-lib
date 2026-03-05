e733a22aedaaa243cdc4d2b2cf9991b66f2579bc
EarthGman <EarthGman@protonmail.com>
2026-03-05 14:36:49 -0600

    add bottles to programs

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -3,6 +3,6 @@
   nixos-installer-x86_64 = lib.mkNixosHost {
     hostname = "nixos-installer";
     system = "x86_64-linux";
-    configuration = ./nixos-installer/configuration.nix;
+    configDir = ./nixos-installer;
   };
 }
