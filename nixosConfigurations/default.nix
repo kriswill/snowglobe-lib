cc75fdf92eeac2712b44e09d94b45d34330e33cb
EarthGman <EarthGman@protonmail.com>
2026-03-04 21:53:31 -0600

    remove temporary nixos configuration

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -5,16 +5,4 @@
     system = "x86_64-linux";
     configuration = ./nixos-installer/configuration.nix;
   };
-
-  nixos = lib.mkNixosHost {
-    hostname = "nixos";
-    cpu-vendor = "amd";
-    firmware = "BIOS";
-    gpu-vendors = [ ];
-    system = "x86_64-linux";
-    desktop = "niri";
-    isQemu = true;
-    configuration = ./nixos/configuration.nix;
-    sopsFile = null;
-  };
 }
