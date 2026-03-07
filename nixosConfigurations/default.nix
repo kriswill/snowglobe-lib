b1030d38a54925b93724c0d53614eaa90b67e4cd
EarthGman <EarthGman@protonmail.com>
2026-03-07 13:32:03 -0600

    refactor build-installers.sh

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -5,4 +5,13 @@
     system = "x86_64-linux";
     configDir = ./nixos-installer;
   };
+
+  nixos-installer-x86_64-small = lib.mkNixosHost {
+    hostname = "nixos-installer";
+    system = "x86_64-linux";
+    configDir = ./nixos-installer;
+    modules = [
+      { hardware.enableRedistributableFirmware = lib.mkForce false; }
+    ];
+  };
 }
--- scripts/build-installers.sh
+++ scripts/build-installers.sh
@@ -8,22 +8,19 @@ if ! ping -c 1 $WEBSITE_IP; then
 	exit 1
 fi
 
-type nixos-generate || {
-	echo "nixos-generate not installed, You can install it through the nix package \"nixos-generators\""
-	exit 1
-}
-
 INSTALLERS=$(
 	for configuration in $(nix eval "$REPO_DIR#nixosConfigurations" --apply builtins.attrNames | sed 's/[][]//g' | tr -d '"'); do
 		printf "%s\n" "$configuration"
 	done | grep 'installer'
 )
 
+mkdir -p "$XDG_CACHE_HOME/nix-modules-CI/installers"
+
 for image in $INSTALLERS; do
 	# store isos so they can be shared between hosts
 	ISO_DEST_PATH=~/src/isos/$image.iso
 	HASH_DEST_PATH="$XDG_CACHE_HOME/nix-modules-CI/installers/$image.iso.sha256"
-	nixos-generate --flake .\#"$image" -f iso -o result || {
+	nixos-rebuild build-image --image-variant iso-installer --flake .\#"$image" || {
 		echo "Could not build the installer image"
 		exit 1
 	}
@@ -37,6 +34,6 @@ for image in $INSTALLERS; do
 	sha256sum "$ISO_DEST_PATH" | cut -d ' ' -f1 >"$HASH_DEST_PATH"
 	gpg --sign --default-key 'EarthGman@protonmail.com' "$HASH_DEST_PATH"
 
-	scp "$ISO_DEST_PATH" "$HASH_DEST_PATH"".gpg" "root@$WEBSITE_IP:/srv/binaries"
+	scp "$ISO_DEST_PATH" "$HASH_DEST_PATH"".gpg" "root@$WEBSITE_IP:/srv/nixos-installers"
 	rm "$HASH_DEST_PATH"
 done
