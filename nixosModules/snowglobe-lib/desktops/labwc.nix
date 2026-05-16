7d08d5b09c719e7c18aefc699e44cb7807edc783
EarthGman <EarthGman@protonmail.com>
2026-05-16 09:46:16 -0500

    add default programs to labwc

--- nixosModules/snowglobe-lib/desktops/labwc.nix
+++ nixosModules/snowglobe-lib/desktops/labwc.nix
@@ -6,12 +6,19 @@
 }:
 let
   cfg = config.snowglobe-lib.desktop.labwc;
+  slib = import ../../../lib/functions/module-wrappers { inherit lib; };
 in
 {
   options.snowglobe-lib.desktop.labwc.enable = lib.mkEnableOption "Snowglobe-Lib's labwc module";
 
   config = lib.mkIf cfg.enable {
     snowglobe-lib.desktop.enable = true;
-    programs.labwc.enable = true;
+    programs = {
+      labwc.enable = true;
+      # default terminal
+      foot.enable = slib.setDefault true;
+      # applications and dmenu
+      rofi.enable = slib.setDefault true;
+    };
   };
 }
