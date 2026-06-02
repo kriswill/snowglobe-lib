c855f9331a11d8ee99612ac54fcf77ae69cfc920
EarthGman <EarthGman@protonmail.com>
2026-06-02 08:54:02 -0400

    disable custom font packages

--- packages/default.nix
+++ packages/default.nix
@@ -4,8 +4,8 @@ let
 in
 {
   nixos-grub-theme = callPackage ./nixos-grub-theme { };
-  omori-font = callPackage ./omori-font { };
-  _8-bit-operator-font = callPackage ./8-bit-operator-font { };
+  # omori-font = callPackage ./omori-font { };
+  # _8-bit-operator-font = callPackage ./8-bit-operator-font { };
   star-pixel-icons = callPackage ./star-pixel-icons { };
   steamServers = import ./steamServers { inherit pkgs; };
 }
