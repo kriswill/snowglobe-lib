10b04d9db1cfbc380e5298ff8b2fe20a9be6c27e
EarthGman <EarthGman@protonmail.com>
2026-06-03 18:09:28 -0400

    disable cache until it comes back online

--- nixosModules/snowglobe-lib/default.nix
+++ nixosModules/snowglobe-lib/default.nix
@@ -48,7 +48,8 @@ in
       # Warning: Personal server for ensuring package patches in overlays/package-patches get cached.
       # Users have the option to enable or disable this per-host depending on the installer type used.
       "nix-store.earthgman.dev" = {
-        enable = slib.setDefault true;
+        # TODO re-enable when the server comes back online
+        enable = slib.setDefault false;
         publicKey = "nix-store.earthgman.dev:2Qrw9kS+K2c00ikcgaz5Y0M7j5XmkhFJz3d7oNgJdLw=";
         # set lower priority than cache.nixos.org by default
         priority = slib.setDefault 100;
